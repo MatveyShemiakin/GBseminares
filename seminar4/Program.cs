ef02ac9003be896ae0d688fa13d6dfc017191f2f
Your Name <you@example.com>
2023-03-08 19:44:38 +0300

    task26

---
+++ seminar4/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+        int GetCountDigit(int number)
+        {
+            int count = 0;
+            int numberA = number;
+            while (numberA != 0)
+            {
+                numberA = numberA / 10;
+                count++;
+            }
+
+            return count;
+        }
+        Console.Write("Ввидите число:");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write($"Количество цифр {GetCountDigit(A)}");
+    }
+}
